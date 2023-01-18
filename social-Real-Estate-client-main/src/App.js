2d9fd80ad889d2fdaf558e1c51b8da65122105e3
talhaunas1 <mtalha4600@gmail.com>
2023-01-18 20:32:44 +0500

    initial commit

---
+++ social-Real-Estate-client-main/src/App.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import Routes from './routes/Routes';
+
+function App() {
+  return (
+    <Container>
+      <Routes />
+    </Container>
+  );
+}
+
+export default App;
+
+
+const Container = styled.div`
+  
+`
